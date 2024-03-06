73ec6c5ad0d3b9b74bb7d7bbacb71b1a392844ee
tleath967 <116659630+tleath967@users.noreply.github.com>
2024-03-06 13:58:57 +0000

    Adjusted the model to predict 10 minutes into the future. Added file that simulates how the ML component of the HPA would work

--- Model/lstmModelTraining.py
+++ Model/lstmModelTraining.py
@@ -4,13 +4,11 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.optimizers import Adam
+from joblib import dump
 
 # Load and preprocess data with date range filtering
 def load_and_preprocess_data(filepath, train_start_date, train_end_date, test_start_date, test_end_date):
     data = pd.read_csv(filepath, parse_dates=['period'], index_col='period')
-
-    # # Filter the dataset for every 2 minutes
-    # data = data[::2] 
 
     train_data = data[train_start_date:train_end_date]
     test_data = data[test_start_date:test_end_date]
@@ -18,17 +16,19 @@ def load_and_preprocess_data(filepath, train_start_date, train_end_date, test_st
     scaler = MinMaxScaler(feature_range=(0, 1))
     train_scaled = scaler.fit_transform(train_data['count'].values.reshape(-1, 1))
     test_scaled = scaler.transform(test_data['count'].values.reshape(-1, 1))
+
+    dump(scaler, 'scaler.joblib')
     
     return train_scaled, test_scaled, scaler
 
 
 # Function to create dataset for LSTM
-def create_dataset(data, look_back=1):
+def create_dataset(data, look_back=1, steps_ahead=10):
     X, Y = [], []
-    for i in range(len(data)-look_back-1):
+    for i in range(len(data)-look_back-steps_ahead):
         a = data[i:(i+look_back), 0]
         X.append(a)
-        Y.append(data[i + look_back, 0])
+        Y.append(data[i + look_back + steps_ahead - 1, 0])
     return np.array(X), np.array(Y)
 
 # Define the LSTM model
@@ -48,17 +48,19 @@ def train_and_test_model(model, X_train, y_train, X_test, y_test):
 def process_file(filepath, train_start_date, train_end_date, test_start_date, test_end_date, look_back=10):
     train_scaled, test_scaled, scaler = load_and_preprocess_data(filepath, train_start_date, train_end_date, test_start_date, test_end_date)
     
-    # Prepare datasets
-    X_train, y_train = create_dataset(train_scaled, look_back)
-    X_test, y_test = create_dataset(test_scaled, look_back)
     
-    # Reshape input for LSTM
+    steps_ahead = 10 # sets how far infornt the predictions want to be (10 minytes in this case)
+    X_train, y_train = create_dataset(train_scaled, look_back, steps_ahead)
+    X_test, y_test = create_dataset(test_scaled, look_back, steps_ahead)
+
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
     X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
     
     # Build and train model
     model = build_model(look_back)
     train_and_test_model(model, X_train, y_train, X_test, y_test)
+
+    model.save('lstm_model.h5')
 
 # Define your date ranges for training and testing
 train_start_date = '1998-06-06 21:30:00'
